b92f7f05e401b87df0ff6d073356fe8592795051
n1g3ld0ugla5 <109959738+n1g3ld0ugla5@users.noreply.github.com>
2022-08-02 14:45:37 +0100

    Create agent-install.sh

---
+++ Automation/agent-install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+export SYSDIG_ACCESS_KEY="****"
+export SAAS_REGION="eu1"
+export COLLECTOR_ENDPOINT="ingest-eu1.app.sysdig.com"
+export COLLECTOR_PORT="6443"
+export API_ENDPOINT="eu1.app.sysdig.com"
+export K8S_CLUSTER_NAME="nigel-eks-cluster2"
+export CLUSTER_REGION="eu-west-1"
+export NODEGROUP_NAME="${K8S_CLUSTER_NAME}-nodegroup"
+export PASSPHRASE="helloworld"
+export INSTANCETYPE="m4.xlarge"
+export NODES="1"
+
+eksctl create cluster -n ${K8S_CLUSTER_NAME} \
+  --nodegroup-name ${NODEGROUP_NAME} \
+  --region ${CLUSTER_REGION} \
+  --node-type ${INSTANCETYPE} \
+  --nodes ${NODES}
+
+helm repo update
+
+helm install sysdig sysdig/sysdig-deploy \
+  --namespace sysdig-agent \
+  --create-namespace \
+  --set global.sysdig.accessKey=${SYSDIG_ACCESS_KEY} \
+  --set global.sysdig.region=${SAAS_REGION} \
+  --set global.clusterConfig.name=${K8S_CLUSTER_NAME} \
+  --set nodeAnalyzer.secure.vulnerabilityManagement.newEngineOnly=true \
+  --set nodeAnalyzer.nodeAnalyzer.apiEndpoint=${API_ENDPOINT} \
+  --set global.kspm.deploy=true \
+  --set agent.sysdig.settings.collector=${COLLECTOR_ENDPOINT} \
+  --set agent.sysdig.settings.collector_port=${COLLECTOR_PORT} \
+  --set agent.sysdig.settings.drift_killer.enabled=true \
+  --set agent.slim.enabled=true
+
+helm install rapid-response sysdig/rapid-response \
+  --namespace sysdig-rapid-response \
+  --create-namespace \
+  --set sysdig.accessKey=${SYSDIG_ACCESS_KEY} \
+  --set rapidResponse.passphrase=${PASSPHRASE}
+
+echo "The rapid response passphrase is ${PASSPHRASE}"
