d1a8b206aff129f4e670f49015e1b8a1a9d33ca7
Nigel Douglas <109959738+n1g3ld0ugla5@users.noreply.github.com>
2022-08-10 11:29:18 +0100

    Update activity.sh

--- Automation/activity.sh
+++ Automation/activity.sh
@@ -1,19 +1,16 @@
 read -p "Create the new workloads"
 kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/master/release/kubernetes-manifests.yaml
-read -p "Check to see if the pods are running"
-kubectl get pods -w
-read -p "Bash exec into the pod"
-kubectl exec -i -t $(kubectl get pod -l "app=emailservice" -o jsonpath='{.items[0].metadata.name}') -- bash
-read -p "Lets get out of this pod"
-exit
-read -p "Automatically generates the Terminal shell in container activity"
+read -p "Check if pods are running - (kubectl get pods)"
+kubectl get pods
+read -p "Check the IP of each pod - (kubectl get pods -o wide)"
+kubectl get pods -o wide
+read -p "Check the label of each pod - (kubectl get pods --show-labels)"
+kubectl get pods --show-labels
+read -p "Bash into the pod based on label - (kubectl exec -i -t $(kubectl get pod -l "app=emailservice" -o jsonpath='{.items[0].metadata.name}') -- /bin/sh)"
 kubectl exec -i -t $(kubectl get pod -l "app=emailservice" -o jsonpath='{.items[0].metadata.name}') -- /bin/sh
-echo "We successfully shelled in to the email pod"
-#10 seconds to observe terminal shell activity
-read -p "Press enter to continue"
-#Each row in /proc/$PID/maps describes a region of contiguous virtual memory in a process or thread.
+
+read -p "Each row in /proc/$PID/maps describes a region of contiguous virtual memory in a process or thread."
+read -p "Run (cat /proc/1/maps) for some verbose outputs "
 cat /proc/1/maps
-echo "Each row in /proc/$PID/maps describes a region of contiguous virtual memory in a process or thread."
-#10 more seconds before exiting the pod
-read -p "Press enter to continue"
+read -p "Leave the pod by running (exit) and hit Enter"
 exit
